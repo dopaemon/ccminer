d68847bc27386514a26494f80cac1ecf9c73627d
dopaemon <polarisdp@gmail.com>
2024-11-05 21:09:16 +0700

    script: Add Name Node
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # ~/ccminer/ccminer -c ~/ccminer/config.json
-~/ccminer/ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RCdEka2mbYDGXQjj79mYvQRNjbEnYUVqva -t 8 --user="A1"
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RCdEka2mbYDGXQjj79mYvQRNjbEnYUVqva.A2 -t 8 --nicehash
