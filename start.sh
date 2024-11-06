1af955d235f9cbfcd9cc7bc1fd8bf3b70cec6226
dopaemon <polarisdp@gmail.com>
2024-11-07 02:44:58 +0700

    start: set pool is zergpool
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # ~/ccminer/ccminer -c ~/ccminer/config.json
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RCdEka2mbYDGXQjj79mYvQRNjbEnYUVqva.A2 -t 8 --nicehash
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RCdEka2mbYDGXQjj79mYvQRNjbEnYUVqva -p c=VRSC,mc=VRSC,ID=A1 -t 8 --nicehash
