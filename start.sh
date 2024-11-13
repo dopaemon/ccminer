9b28170435993b10bb9b8e99748e1e7c32215940
dopaemon <polarisdp@gmail.com>
2024-11-13 14:04:52 +0700

    start: using full cpu
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # ~/ccminer/ccminer -c ~/ccminer/config.json
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RCdEka2mbYDGXQjj79mYvQRNjbEnYUVqva.A2 -t 8 --nicehash
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RCdEka2mbYDGXQjj79mYvQRNjbEnYUVqva.A -t $(nproc --all) --nicehash
